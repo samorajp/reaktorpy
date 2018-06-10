e109db2264386dd71da9e0e4cdc7b538e1db95df
michal.a <mj_adamski@wp.pl>
2018-06-10 12:36:17 +0200

    zadanka 13-19

---
+++ michal/dzien1/p14.py
@@ -0,0 +1,11 @@
+a = 7
+b = 0
+c = None
+d = ""
+e = ''
+
+print(a, bool(a))
+print(b, bool(b))
+print(c, bool(c))
+print(d, bool(d))
+print(e, bool(e))
---
+++ michal/dzien1/p19.py
@@ -0,0 +1,11 @@
+imie = "Michał"
+nazwisko = "Adamski"
+data_ur = "31.01.1986"
+stanowsiko = "bezrobotny"
+placa = 0
+
+wyswietl = imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa)
+
+#print(imie, nazwisko, data_ur, stanowsiko, placa, sep=" - " )
+print(wyswietl*10)
+
