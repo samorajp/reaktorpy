3f6dd3103683a33895de3144e97ee4467840dc99
michal.a <mj_adamski@wp.pl>
2018-06-10 11:18:12 +0200

    9

--- michal/dzien1/p9.py
+++ michal/dzien1/p9.py
@@ -4,6 +4,14 @@ vat7 = 0.07
 vat23 = 0.23
 
 print("kwota brutto", kwota)
-print("netto przy vat 3%", round(kwota - kwota * vat3, 2))
-print("netto przy vat 7%", round(kwota - kwota * vat7, 2))
-print("netto przy vat 23%", round(kwota - kwota * vat23, 2))
+print("netto przy vat 3%", round(kwota / (1 + vat3), 2))
+print("netto przy vat 7%", round(kwota / (1 + vat7), 2))
+print("netto przy vat 23%", round(kwota / (1 + vat23), 2))
+
+print("\n")
+brutto = 999
+stawka = 0.03
+netto = brutto * stawka
+zaokraglone = round(netto, 2)
+
+print(zaokraglone)
