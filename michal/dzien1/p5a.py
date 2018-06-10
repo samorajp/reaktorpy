dadd638cd2119632e4dee7463d1724e47340d116
michal.a <mj_adamski@wp.pl>
2018-06-10 10:33:14 +0200

    5a dodatkowe ćwiczenie

---
+++ michal/dzien1/p5a.py
@@ -0,0 +1,17 @@
+imie = ""
+nazwisko = ""
+data_ur = None
+stanowsiko = ""
+placa = None
+
+
+print (imie, nazwisko, data_ur, stanowsiko, placa, sep=" - ")
+
+imie = "Michał"
+nazwisko = "Adamski"
+data_ur = "31.01.1986"
+stanowsiko = "bezrobotny"
+placa = 0
+
+print("\n")
+print (imie, nazwisko, data_ur, stanowsiko, placa, sep=" - ")
