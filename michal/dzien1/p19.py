d5918d892fdc3a85c1054630384fac86cf306f68
michal.a <mj_adamski@wp.pl>
2018-06-10 13:02:50 +0200

    update 19a

--- michal/dzien1/p19.py
+++ michal/dzien1/p19.py
@@ -4,10 +4,15 @@ data_ur = "31.01.1986"
 stanowsiko = "bezrobotny"
 placa = 0
 
-wyswietl = imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa)
+wyswietl = imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa) + "\n"
 
-print(imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa)*10)
-print((imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa))*10)
-print(imie, nazwisko, data_ur, stanowsiko, str(placa), sep=" - " )
-print(wyswietl*10)
+print("1" + imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa)*10)
+print("2" + (imie + " " + nazwisko + " " + data_ur + " " + stanowsiko + " " + str(placa))*10)
+print("3" + imie, nazwisko, data_ur, stanowsiko, str(placa), sep=" - " )
+print(("4" + imie, nazwisko, data_ur, stanowsiko, str(placa))*10, sep=" - " )
 
+a = '999'
+print('a')
+print(print(wyswietl*10), sep="\n")
+
+print(print(wyswietl, sep="\n")*10)
