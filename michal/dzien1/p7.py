9450dcc9604dc6383c5bcf0862b1a1c127049c5a
michal.a <mj_adamski@wp.pl>
2018-06-10 11:08:44 +0200

    zadanka 7-9

---
+++ michal/dzien1/p7.py
@@ -0,0 +1,15 @@
+imie = "Michał"
+nazwisko = "Adamski"
+data_ur = "31.01.1986"
+stanowsiko = "bezrobotny"
+placa = 0
+
+
+print (imie, nazwisko, data_ur, stanowsiko, placa, sep=" - ")
+
+print ("\n")
+print (type(imie))
+print (type(nazwisko))
+print (type(data_ur))
+print (type(stanowsiko))
+print (type(placa))
