9f5e81ea1a11c6252674afa6f16b901b0ca3c066
PawelKowalewski1 <p.m.kowalewski@gmail.com>
2018-06-10 11:33:36 +0200

    p1-p12 commit 3

---
+++ pawel/dzien1/p10.py
@@ -0,0 +1,10 @@
+chleb=1.99 #PLN/szt
+mleko= 2.50 #PLN/litr
+cukierki=12.99 #PLN/kg
+ilosć_chleba=1
+ilość_mleka=0.5
+ilość_cukierków=0.3
+
+zamówienie = chleb*ilosć_chleba+mleko*ilość_mleka+cukierki*ilość_cukierków
+
+print(round(zamówienie, 2))
