d4160bf7fb7f81cb8d6b78ac6ee07195dc501e3a
Dawid949PWN <40119829+Dawid949PWN@users.noreply.github.com>
2018-06-14 23:05:48 +0200

    commit_zadanie

---
+++ dawid/dzien1/p26.py
@@ -0,0 +1,11 @@
+#zadanie 26
+
+Z = 17 % 3
+b = Z+3
+wynik = b * Z
+print(wynik)
+
+# alternatywne rozwiązanie
+Z = 17 % 3
+wynik = Z * Z + 3 * Z
+print(wynik)
---
+++ dawid/dzien1/p32.py
@@ -0,0 +1,12 @@
+
+#zadanie32
+
+podstawa = int(input("Podaj podstawy trójkąta długość : \n"))
+wysokosc = int(input("Podaj podstawy trójkąta wysokość : \n"))
+
+
+pole = (podstawa * wysokosc)/2
+print("Pole = ", pole)
+
+
+
