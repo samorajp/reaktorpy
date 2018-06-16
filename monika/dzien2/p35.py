73540d17b7e8a21e587af26ae32d4aca40070115
monikabajer <monika.bajer@op.pl>
2018-06-16 12:22:36 +0200

    napisy * 10

---
+++ monika/dzien2/p35.py
@@ -0,0 +1,19 @@
+liczby = []
+liczby.append(1)
+liczby.append(2)
+liczby.append(3)
+
+imiona = []
+imiona.append("Adam")
+imiona.append("Ewa")
+
+imie = imiona[1]
+print(imie)
+
+#p36
+
+owoce = ["mango", "gruszka", "kiwi", "truskawka", "banan"]
+
+print(owoce[0])
+print(owoce[4])
+print(owoce[-1])
---
+++ monika/dzien2/p37.py
@@ -0,0 +1,21 @@
+slowa = 'Witaj', 'w', 'kursie', 'Python'
+
+print(slowa)
+print(slowa[0],  slowa[1], slowa[2], slowa[3])
+print(slowa[1])
+print(slowa[2])
+print(slowa[3])
+
+
+import datetime
+dzis = datetime.datetime.now()
+dzis.
+
+
+
+#p38
+
+krotka = '16-','06-','2018'
+print("Data waznosci artykulu:", krotka[0],krotka[1],krotka[2])
+
+
