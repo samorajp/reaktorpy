db795a5ee45e6c3b2974cebc21e28ffa6f568fab
jakub.wojcik <jakubwojcik2@gmail.com>
2018-06-16 15:32:01 +0200

    DrugiDzień

---
+++ kuba/dzien2/P44.py
@@ -0,0 +1,9 @@
+napis1 = input('Wpisz pierwszą liczbę: ')
+napis2 = input('Wpisz drugą liczbę: ')
+napis3 = input('Wpisz trzecią liczbę: ')
+
+
+#print(len(set([napis1,napis2,napis3])))
+zbiór = set([napis1, napis2, napis3])
+ile = len(zbiór)
+print(ile)
