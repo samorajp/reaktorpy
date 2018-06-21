45d157a08324ef9f59bfab0b71441381a3da119a
Dawid949PWN <40119829+Dawid949PWN@users.noreply.github.com>
2018-06-22 01:36:30 +0200

    py_commit

---
+++ dawid/dzien3/dodatkowe_domowep60.py
@@ -0,0 +1,17 @@
+# (P60)
+
+liczba1 = None
+
+napis = input("Podaj liczbe1: ")
+if napis. isdigit():
+    liczba1 = int(napis)
+
+liczba2 = None
+napis = input("Podaj liczbe2: ")
+if napis.isdigit():
+    liczba2= int(napis)
+
+print("\n" * 1)
+print("Wynik miedzy dwoma punkty liczby odległości:",
+(liczba1 + liczba2) / 2,str("km"))
+
