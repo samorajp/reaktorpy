26e0f121a50bd641bcbd64a8e5eeffbbff5d20bc
Dawid949PWN <40119829+Dawid949PWN@users.noreply.github.com>
2018-06-21 15:03:05 +0200

    commit_zadanie

---
+++ dawid/dzien2/p46.py
@@ -0,0 +1,9 @@
+# p46 zadanie
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+if float(input("Podaj liczbe: ")) in lista:
+    print("w przedziale")
+else:
+    print("out of range")
+
+
---
+++ dawid/dzien2/p48.py
@@ -0,0 +1,8 @@
+# p48 zadanie
+liczba = int(input("Podaj liczbe: "))
+
+if ...:
+    print("Parzysta ")
+else:
+    print("Nieparzysta ")
+
