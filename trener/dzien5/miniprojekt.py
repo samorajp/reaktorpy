bc4ad255739b1072099e7794fadabb013192e4d2
Paweł Samoraj <samoraj.pawel@gmail.com>
2018-06-24 17:02:21 +0200

    koniec naszego miniprojektu

--- trener/dzien5/miniprojekt.py
+++ trener/dzien5/miniprojekt.py
@@ -50,19 +50,43 @@ def zobacz_oferte():
 
 
     for produkt in oferta:
-        print("%10s  %5s" % (produkt[1], produkt[2]))
+        print("%3s %10s  %5s" % (produkt[0], produkt[1], produkt[2]))
     print()
 
-def zloz_zamowienie():
+def zloz_zamowienie(login):
     print("Składamy zamówienie")
+
+    kod = input("Podaj kod zamówienia: ")
+
+    while True:
+        zobacz_oferte()
+        kod_produktu = input("Wprowadź kod produktu:")
+        ilosc = input("Podaj ilość: ")
+
+        zapytanie = """
+            insert into zamowienia (login, kod_zamowienia, id_produktu, ilosc)
+            values (%s, %s, %s, %s)
+        """
+
+        c.execute(zapytanie, (login, kod, kod_produktu, ilosc))
+        conn.commit()
+
+        odp = input("Czy chcesz dalej kupować? [tak/nie]")
+
+        if odp != "tak":
+            break
+
+    print("Zamównienie o kodzie %s zostało złożone" % kod)
 
 def historia_zamowien(login):
     print("Historia zamówień poniżej.")
 
     zapytanie = """
-        select zamowienia.kod_zamowienia,  produkty.nazwa, zamowienia.ilosc, produkty.cena
+        select zamowienia.kod_zamowienia, 
+        produkty.nazwa, zamowienia.ilosc, produkty.cena
         from zamowienia, produkty
-        where login = %s and zamowienia.id_produktu = produkty.id_produktu
+        where login = %s and 
+        zamowienia.id_produktu = produkty.id_produktu
     """
 
     c.execute(zapytanie, login)
@@ -71,72 +95,64 @@ def historia_zamowien(login):
     for pozycja in pozycje:
         print(pozycja)
 
-def dodaj_produkt(typ_konta):
-    if typ_konta == "A":
-        ...
+def dodaj_produkt():
+    nazwa = input("Podaj nazwę nowego produktu: ")
+    cena = input("Podaj cenę nowego produktu: ")
+
+    zapytanie = """
+        insert into produkty (nazwa, cena) values (%s, %s)
+    """
+
+    c.execute(zapytanie, (nazwa, cena))
+    conn.commit()
+
+    print("Pomyślnie dodano produkt: %s" % nazwa)
 
 
 
 
 def menu(login, typ_konta):
-    print("## MENU, witaj %s ##" % login)
-    print("1 - Wyloguj się")
-    print("2 - Zobacz ofertę")
-    print("3 - Złóż zamówienie")
-    print("4 - Historia zamówień")
-    print("5 - D")
+    if typ_konta == "U":
+        print("## MENU, witaj %s ##" % login)
+        print("1 - Wyloguj się")
+        print("2 - Zobacz ofertę")
+        print("3 - Złóż zamówienie")
+        print("4 - Historia zamówień")
+        print("5 - D")
 
-    akcja = input("Co chcesz zrobić? ")
+        akcja = input("Co chcesz zrobić? ")
 
-    if akcja == "1":
-        wyloguj_sie()
-    elif akcja == "2":
-        zobacz_oferte()
-    elif akcja == "3":
-        zloz_zamowienie()
-    elif akcja == "4":
-        historia_zamowien(login)
-    else:
-        print("Nie wiem o co Ci chodzi.")
-    menu(login, typ_konta)
+        if akcja == "1":
+            wyloguj_sie()
+        elif akcja == "2":
+            zobacz_oferte()
+        elif akcja == "3":
+            zloz_zamowienie(login)
+        elif akcja == "4":
+            historia_zamowien(login)
+        else:
+            print("Nie wiem o co Ci chodzi.")
+        menu(login, typ_konta)
+    elif typ_konta == "A":
+        print("1 - Wyloguj")
+        print("2 - Dodaj produkt")
+        print("3 - ...")
+        print("4 - ...")
+
+        akcja = input("Co chcesz zrobić? ")
+
+        if akcja == "1":
+            wyloguj_sie()
+        elif akcja == "2":
+            dodaj_produkt()
+        else:
+            print("Nie wiem o co Ci chodzi.")
+        menu(login, typ_konta)
+
 
 
 
 login, typ_konta = ekran_logowania_do_skutku()
+
+
 menu(login, typ_konta)
-
-
-
-
-
-
-
-
-
-
-menu()
-exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-zalogowany_user, typ_konta = ekran_logowania_do_skutku()
-
-if typ_konta == "A":
-    podany_login = input("Podaj login nowego użytkownika: ")
-    podane_haslo = input("Hasło dla nowego użytkownika: ")
-
-    c.execute("INSERT INTO logowanie VALUES (%s, %s, %s)", (podany_login, podane_haslo, 'U'))
-    conn.commit()
-else:
-        pass
